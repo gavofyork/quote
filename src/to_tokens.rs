b700ce3b7c870239ba613c04f1c628ea3d120477
Gav <gavin@parity.io>
2018-05-15 11:23:21 +0200

    Add 128-bit support.

--- src/to_tokens.rs
+++ src/to_tokens.rs
@@ -117,12 +117,14 @@ primitive! {
     i16 => i16_suffixed
     i32 => i32_suffixed
     i64 => i64_suffixed
+    i128 => i128_suffixed
     isize => isize_suffixed
 
     u8 => u8_suffixed
     u16 => u16_suffixed
     u32 => u32_suffixed
     u64 => u64_suffixed
+    u128 => u128_suffixed
     usize => usize_suffixed
 
     f32 => f32_suffixed
